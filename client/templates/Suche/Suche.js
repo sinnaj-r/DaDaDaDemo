f7a532a035ce872cbfb2b1ee5797eb45440ea107
Jannis Rosenbaum <j.ro@me.com>
2016-10-16 01:14:26 +0200

    Suche BL Filter

--- client/templates/Suche/Suche.js
+++ client/templates/Suche/Suche.js
@@ -1,14 +1,19 @@
 Template.Suche.onCreated(function(){
     Session.set("search_string","")
+    Session.set("search_bl","")
 })
 
 Template.Suche.helpers({
     filterO:function(){
         let search_string = Session.get("search_string")
+        let search_bl = Session.get("search_bl")
         if (search_string != ""){
             var filter = { $or: [{"ueberschrift" : {$regex : search_string}},{"beschreibung" : {$regex : search_string}}] }
             } else {
                 var filter = {}
+            }
+        if (search_bl != ""){
+            filter = {$and:[filter,{"ort.1" : {$regex : search_bl}}]}
             }
         //console.log("Hallo",filter)
         return filter
@@ -19,5 +24,8 @@ Template.Suche.events({
     "keyup #search_input":function(event){
         let search_string = $("#search_input").val()
         Session.set("search_string",search_string)
+    },
+    "select #bl_select":function(){
+        Session.set("search_bl",search_string)
     }
 })
