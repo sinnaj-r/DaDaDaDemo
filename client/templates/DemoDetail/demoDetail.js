095c7afc92c647084d6a5b691818f0a27f92436c
Jannis Rosenbaum <j.ro@me.com>
2016-10-16 01:00:11 +0200

    Beschreibung wird Abgekürzt (wenn nötig)

--- client/templates/DemoDetail/demoDetail.js
+++ client/templates/DemoDetail/demoDetail.js
@@ -1,3 +1,9 @@
 Template.DemoDetail.helpers({
-
+    "beschreibung_kurz":function(){
+        var besch = this.beschreibung
+        if (this.beschreibung.length > 140){
+            besch=this.beschreibung.substring(0, 140)+"[...]"
+        }
+        return besch
+    }
 });
