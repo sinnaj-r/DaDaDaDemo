5002f946c05a8b9a0a64fbb1d405a8329a7edb33
Jannis Rosenbaum <j.ro@me.com>
2016-10-15 11:38:12 +0200

    Initaler Commit

---
+++ startup/collections.js
@@ -0,0 +1,50 @@
+Events = new Mongo.Collection("Events");
+
+Events.attachSchema(new SimpleSchema({
+    ueberschrift:{
+        type: String,
+        label: "Überschrift",
+    },
+    beschreibung:{
+        type: String,
+        label: "Beschreibung",
+    },
+    ort: {
+        type: String,
+        label: "_id des Ortes",
+    },
+    datum: {
+        type: [Number],
+        label: "Von bis UnixDate",
+    },
+    valueOferanstalter: {
+        type: [String],
+        label: "Array aus den Veranstalter _id's",
+    },
+    teilnehmerzahl:{
+        type: String,
+        label: "Kategorie",
+        autoValue: function() {
+            var content = this.field("teilnehmer");
+            if (content.isSet) {
+                return content.length;
+            } else {
+                return 0;
+            }
+        }
+    },
+    twitter:{
+        type: String,
+        label: "Twitter Hashtag",
+        required:false
+    },
+    kategorie:{
+        type: [String],
+        label: "Array der Kategorien",
+        required:false
+    },
+    teilnehmer:{
+        type: [String],
+        label: "Array der Teilnehmer _ids",
+    }
+}))
