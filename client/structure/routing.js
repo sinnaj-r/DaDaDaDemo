66d4a1c4a1e583ffa04915867b741c25dbd64733
Jannis Rosenbaum <j.ro@me.com>
2016-10-15 14:53:58 +0200

    Join Event Methode hinzugefügt

--- client/structure/routing.js
+++ client/structure/routing.js
@@ -17,3 +17,34 @@ Router.route('/gentestdata', function () {
     Session.set("DocumentTitle",'Home');
     this.render('GenTestData');
 });
+
+
+Router.route('/demo/:_id', {
+    data:function () {
+        var item = Events.findOne({
+            _id: this.params._id
+        });
+        if(!item){Router.go("/404")}
+        return item
+    },
+    onAfterAction:function(){
+        d = this.data()
+        if (d){
+            Session.set("DocumentTitle",`Event - ${d.ueberschrift}`);
+            return true
+        }else{
+            return false
+        }
+    },
+    action:function(){
+        if (this.ready()) {
+            this.render("demoDetail")
+        }
+        else{
+            this.render('loading');
+        }
+    },
+    waitOn: function(){
+        return Meteor.subscribe("Events")
+    }
+});
